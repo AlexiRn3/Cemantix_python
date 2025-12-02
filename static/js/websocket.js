ea87c5c0d8562be0ba5037bc8ce9a5211896d87c
AlexiRn3 <alexi.mathiou@icloud.com>
2025-12-02 13:39:25 +0100

    Update websocket.js

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,7 +1,7 @@
 import { state } from "./state.js";
 import { addEntry, renderHistory, renderScoreboard, triggerConfetti, updateRoomStatus } from "./rendering.js";
 import { addHistoryMessage, setRoomInfo } from "./ui.js";
-import { addChatMessage } from "./main.js";
+import { addChatMessage } from "./chat_ui.js";
 
 export function openWebsocket(playerName) {
     if (!state.currentRoomId) return;
